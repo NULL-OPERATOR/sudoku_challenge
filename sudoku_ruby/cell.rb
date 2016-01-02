1fb0eee4046fd2bc1456855add7b83b416acf8a8
NULL-OPERATOR <x@cryptolab.net>
2016-01-02 01:33:12 +0000

    working logic, needs refactoring/grid setup class

--- sudoku_ruby/cell.rb
+++ sudoku_ruby/cell.rb
@@ -1,8 +1,9 @@
 class Cell
   FULL_SET = [0,1,2,3,4,5,6,7,8,9]
 
-  def solve(relevant_cells)
-    FULL_SET - @relevant_cells
+  def solve(input_cells)
+    output = FULL_SET - input_cells
+    output.length > 1 ? nil : output[0]
   end
 
 end
--- sudoku_ruby/grid.rb
+++ sudoku_ruby/grid.rb
@@ -1,70 +1,98 @@
+require_relative "cell.rb"
+
 class Grid
   attr_reader :cells, :output
 
-  def initialize(input_grid, cell)
-    grid_setup(input_grid)
-    @cell = cell || Cell.new
+  def initialize(input_grid=nil, cell_klass=nil)
+    # grid_setup(input_grid)
+    @boxes = [[1,1],[2,1],[3,1]]
+    grid_setup('015003002000100906270068430490002017501040380003905000900081040860070025037204600')
+    @cell_klass = cell_klass || Cell.new
   end
 
   def solve
-    cells.each do |cell|
-      solved?(cell) ? next : solve_cell(cell)
+    output = cells.map do |cell|
+      if solved?(cell)
+        cell[4]
+      else
+        solved_cell = solve_cell(cell)
+        if solved_cell == nil
+          0
+        else
+          solved_cell[4]
+        end
+      end
+    end
+
+    if output.include?(0)
+      solve
+    else
+      output.join
     end
   end
 
   private
 
   def grid_setup(input_grid)
-
-    boxA, boxB, boxC = [1,1], [2,1], [3,1]
     row, col = 1, 1
-    @cells = input_grid.split(//).map.with_index do |cell, index|
-      box = boxA[0]
-      if col < 4
-        if boxA[1] == 9
-          boxA[0] += 1
-          boxA[1] = 1
-        end
+    id = 0
+    @cells = input_grid.split(//).map do |cell|
 
-      elsif col > 6
-        box = boxC[0]
-        if boxB[1] == 9
-          boxB[0] += 1
-          boxB[1] = 1
-        end
+      box = get_box(col)
 
-      else
-        box = boxB[0]
-        if boxB[1] == 9
-          boxB[0] += 1
-          boxB[1] = 1
-        end
-      end
-
-      a = [row, col, box, cell]
-
+      a = [id, row, col, box, cell.to_i]
+      id += 1
       if col == 9
         row += 1
         col = 1
       else
         col += 1
       end
-
       a
-    # column = multiples of 1-9
-    # plus row no. 1-9, resetting every row
     end
   end
 
-  def solved?(current_cell)
-    current_cell != 0
+  def solved?(cur_cell)
+    cur_cell[4] != 0
   end
 
-  def solve_cell(current_cell)
-    cells = cells_needed(current_cell)
-    @cell.solve(cells)
+  def get_box(col)
+    if col < 4
+      box = @boxes[0][0]
+      box_update(0)
+    elsif col > 6
+      box = @boxes[2][0]
+      box_update(2)
+    else
+      box = @boxes[1][0]
+      box_update(1)
+    end
+    box
   end
 
-  def cells_needed(current_cell)
+  def box_update(box)
+    if @boxes[box][1] == 9
+      @boxes[box][0] += 3
+      @boxes[box][1] = 1
+    else
+      @boxes[box][1] +=1
+    end
   end
+
+
+  def solve_cell(cur_cell)
+    val = @cell_klass.solve(cells_needed(cur_cell))
+    if val == nil
+      return
+    end
+    new_cell = cur_cell
+    new_cell[4] = val
+    @cells[cur_cell[0]] = new_cell
+  end
+
+  def cells_needed(cur_cell)
+    to_add = cells.select{|c| c[1] == cur_cell[1] || c[2] == cur_cell[2] || c[3] == cur_cell[3]}
+    to_add.map!{|x| x[4]}.uniq!
+  end
+
 end
